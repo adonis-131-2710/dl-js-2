4a97f7958adf409b266aa073a37d85737c0f9d5c
Phúc Lợi <huynhphucloi007@gmail.com>
2026-01-04 20:09:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+ function dongho() {
+        var time = new Date();
+        var gio = time.getHours();
+        var phut = time.getMinutes();
+        var giay = time.getSeconds();
+
+        if (gio < 10)
+            gio = "0" + gio;
+
+        if (phut < 10)
+            phut = "0" + phut;
+
+        if (giay < 10)
+            giay = "0" + giay;
+
+        document.getElementById("time").innerHTML = gio + ":" + phut + ":" + giay;
+        setTimeout("dongho()", 1000);
+    }
+    dongho();
